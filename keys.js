7784f3e02d8f2f519189da025f8728bc10111e1c
Evan Kozierachi <evan.kozierachi@gmail.com>
2018-12-01 16:51:10 -0800

    keys & env

--- keys.js
+++ keys.js
@@ -1,6 +1,21 @@
+require('dotenv').config();
+
 console.log('this is loaded');
 
-exports.spotify = {
+var spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+
+// console.log('this is loaded');
+
+// var penv = require("./ks.env");
+// var spotifyKeys = {
+//   id: penv.SPOTIFY_ID,
+//   secret: process.env.SPOTIFY_SECRET
+// };
+
+module.exports = spotify;
+console.log("spotifyKeys: " + spotify);
+// // module.exports.spotifyKeys;
--- liri.js
+++ liri.js
@@ -9,7 +9,73 @@ var fs = require("fs");
 
 // Request for the keys folder
 var keys = require("./keys");
+// console.log("keys: " + keys.spotify.id);
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------
-// Setting up the Spotify search function
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
+
+var spotify = new Spotify(keys);
+
+// var spotify = new Spotify({
+//     id: process.env.SPOTIFY_ID,
+//     secret: process.env.SPOTIFY_SECRET
+//   });
+
+var spotifyThis = function(songTitle) {
+
+	spotify.search({ type: 'track', query: songTitle }, function (err, data) {
+		if (err) {
+			return console.log('Error occurred: ' + err);
+		}
+		var songs = data.tracks.items;
+		for (var i = 0; i < songs.length; i++) {
+			console.log("\n" + "~".repeat(29));
+			console.log(i)
+			console.log("Artist(s): " + songs[i].artists.map(getArtistNames));
+			console.log("Song Name: " + songs[i].name);
+			console.log("Preview Song: " + songs[i].preview_url);
+			console.log("Album: " + songs[i].album.name);
+			console.log("~".repeat(29) + "\n");
+		}
+	});
+
+}
+
+var liriThis = function (caseData, funcData) {
+	switch (caseData) {
+		case "spotify-this-song":
+			if (!funcData) {
+				funcData = "The Sign"
+				spotifyThis(funcData);
+				break;
+			}
+			spotifyThis(funcData);
+            break;
+            
+        case "movie-this":
+            if (!funcData){
+                funcData = "Mr. Nobody";
+				movieThis(functData);
+				break;
+            }
+			movieThis(funcData);
+            break;
+            
+		case "concert-this":
+			concertThis(funcData);
+            break;
+            
+		case "do-what-it-says":
+			doWhatItSays();
+            break;
+            
+		default:
+			console.log("LIRI says whaaat?");
+	}
+}
+
+
+var input1 = process.argv[2];
+var input2 = process.argv.slice(3).join(" ");
+
+liriThis(input1, input2);
